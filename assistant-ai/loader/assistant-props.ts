6e4f386e60d75a14302034bcaa46bf87e8c69f29
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2023-11-05 11:37:22 -0300

    Feat: Message

--- assistant-ai/loader/assistant-props.ts
+++ assistant-ai/loader/assistant-props.ts
@@ -1,5 +1,80 @@
-import type { Product } from "../../commerce/types.ts";
 import { AppContext } from "../mod.ts";
+
+import type { ImageWidget } from "../../admin/widgets.ts";
+import { Suggestion } from "../../commerce/types.ts";
+import { Resolved } from "deco/engine/core/resolver.ts";
+
+export interface AboutBusiness {
+  name: string;
+
+  /**
+   * @title Store segment
+   * @description Tell us what is the segment your store ?
+   */
+  segment: string[];
+
+  /**
+   * @title Address
+   * @description Tell us where is location your store with logradouro, state, city and country.
+   */
+  address: string[];
+
+  /**
+   * @title State Available for Delivery
+   * @description Tell us what states is available for delivery
+   */
+  deliveryStates: string[];
+
+  /**
+   * @title Contact
+   * @description Tell us what phone numbers is available for service
+   */
+  contact: string[];
+
+  /**
+   * @title Payment methods available
+   * @description What payment methods is available, if card is then, specific the flag is available ?
+   */
+  payment: string[];
+
+  /**
+   * @title FAQ
+   * @description Describe what is FAQ
+   */
+  faq: {
+    question: string;
+    answer: string;
+  }[];
+}
+
+export interface ChatConfigs {
+  /**
+   * @title Assistant Icon
+   * @description What is Assistant Icon that you want ?
+   */
+  icon: ImageWidget;
+}
+
+export interface Props {
+  /**
+   * @title ChatGPT Api Key
+   * @description Create account in ChatGpt and access https://platform.openai.com/account/api-keys
+   */
+  keyChatGPT: string;
+
+  /**
+   * @title Business About
+   * @description Tell us about your business
+   */
+  AboutBusiness: AboutBusiness;
+
+  /**
+   * @title Chat Configurations
+   */
+  Chat: ChatConfigs;
+
+  Integration: Resolved<Suggestion | null>;
+}
 
 /**
  * @title Assitant Props
@@ -7,9 +82,9 @@ import { AppContext } from "../mod.ts";
 const loader = (
   _req: Request,
   ctx: AppContext,
-): Promise<Product[] | null> => {
+): Props => {
   const state = ctx;
-
+  console.log(state, "ESTADO");
   return state;
 };
 
